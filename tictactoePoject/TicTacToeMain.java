49c711f9e85c0fa292c936441cfc84b70fce5cc9
Onukwufor dominic <dominicrotimi@gmail.com>
2023-02-12 18:54:13 -0500

    Add files via upload

---
+++ tictactoePoject/TicTacToeMain.java
@@ -0,0 +1,9 @@
+package tictactoePoject;
+
+public class TicTacToeMain {
+    public static void main(String[] args) {
+        TicTacToe ticTacToe = new TicTacToe();
+        ticTacToe.play();
+
+    }
+}
