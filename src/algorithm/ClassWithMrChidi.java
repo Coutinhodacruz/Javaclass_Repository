70994be900285e68f47dee110f149f896fbb1257
onukwufor dominic <dominicrotimi@gmail.com>
2023-07-07 14:51:38 -0400

    commit

---
+++ src/algorithm/ClassWithMrChidi.java
@@ -0,0 +1,48 @@
+package algorithm;
+
+public class ClassWithMrChidi {
+
+//    public static void main(String[] args) {
+//        int[] setOne = {3,1,7,9};
+//        int[] setTwo = {2,4,1,9,3};
+//
+//        System.out.println(sumOfElement(setOne, setTwo));
+//    }
+//
+//
+//
+//    public static int sumOfElement(int[] set1, int[] set2){
+//        int sum = 0;
+//
+////
+//        for (int i = 0; i < set1.length; i++) {
+//            boolean isDistinct = true;
+//            for (int j = 0; j < set2.length; j++) {
+//                if (set1[i] == set2[j]){
+//                    isDistinct = false;
+//                    break;
+//                }
+//            }
+//            if (isDistinct){
+//                sum += set1[i];
+//            }
+//        }
+//        for (int i = 0; i < set2.length; i++) {
+//            boolean isDistinct = true;
+//            for (int j = 0; j < set1.length; j++) {
+//                if (set2[i] == set1[j]){
+//                    isDistinct = false;
+//                    break;
+//                }
+//
+//            }
+//            if (isDistinct){
+//                sum += set2[i];
+//            }
+//
+//        }
+//
+//        return sum;
+//    }
+
+}
